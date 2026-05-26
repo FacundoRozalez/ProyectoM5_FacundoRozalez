ec19c4c058ea9ddf7362d644987a0181bf048308
FacundoRozalez <facurozalez@gmail.com>
2026-05-26 17:31:26 +0000

    feat: cargar dinámicamente el archivo .env usando rutas absolutas en client.ts

--- src/github/client.ts
+++ src/github/client.ts
@@ -1,9 +1,16 @@
 import { Octokit } from "@octokit/rest";
 import { AuthenticationError } from "../errors/index.js";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const envPath = path.resolve(__dirname, "../../.env");
+
+dotenv.config({ path: envPath });
+
 const token = process.env.GITHUB_TOKEN;
-if (!token) throw new AuthenticationError("Falta la variable GITHUB_PERSONAL_ACCESS_TOKEN");
+if (!token) throw new AuthenticationError("Falta la variable GITHUB_TOKEN en el archivo .env");
 
 export const octokit = new Octokit({ auth: token });
